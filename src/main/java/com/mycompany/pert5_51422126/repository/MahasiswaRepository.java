a41fb335a425e78340666c675741f4bd35f63b6c
Raihan <mraihank0@gmail.com>
2025-11-25 14:43:12 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert5_51422126/repository/MahasiswaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert5_51422126.repository;
+
+import com.mycompany.pert5_51422126.model.ModelMahasiswa;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+/**
+ *
+ * @author USER
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Long> {
+
+    public List<ModelMahasiswa> findAll();
+
+}
